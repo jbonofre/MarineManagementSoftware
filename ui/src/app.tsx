65043d73be3bafce867b2fe29fb6f0c85fae1a24
JB Onofré <jb.onofre@dremio.com>
2025-03-23 19:50:11 +0100

    Add login modal

--- ui/src/app.tsx
+++ ui/src/app.tsx
@@ -1,14 +1,50 @@
-import React from 'react';
-import { Button, Space, DatePicker, version } from 'antd';
+import React, { useState } from 'react';
+import type { FormProps } from 'antd';
+import { Modal, Button, Form, Input, Space, Checkbox, message } from 'antd';
 
-const App = () => (
-  <div style={{ padding: '0 24px' }}>
-    <h1>antd version: {version}</h1>
-    <Space>
-      <DatePicker />
-      <Button type="primary">Primary Button</Button>
-    </Space>
-  </div>
-);
+type FieldType = {
+    username?: string;
+    password?: string;
+    remember?: string;
+}
 
-export default App;
+const onFinish: FormProps<FieldType>['onFinish'] = (values) => {
+    console.log('Success:', values);
+};
+
+const onFinishFailed: FormProps<FieldType>['onFinishFailed'] = (values) => {
+    console.log('Failed:', values);
+};
+
+export default function App() {
+
+    const [ user, setUser ] = useState();
+    const [ loginForm ] = Form.useForm();
+
+    if (user) {
+        return(
+            <Space>Hello</Space>
+        );
+    } else {
+        return(
+            <Modal centered mask={false} title="Bienvenue sur ShipyardSo" open={true} okText="Se connecter" cancelText="Effacer" closable={false} onOK={loginForm.submit} onCancel={() => loginForm.resetFields()}>
+                 <Form name="login" form={loginForm} labelCol={{ span: 8 }} wrapperCol={{ span: 16 }} autoComplete="off" onFinish={(values) => {
+                   if (values.username !== 'admin' && values.username !== 'msplaisance' && values.username !== 'franck') {
+                       message.error("Please use valid user");
+                   } else {
+                       setUser(values.username);
+                   }
+                 }} onKeyUp={(event) => {
+                     if (event.keyCode === 13) {
+                       loginForm.submit();
+                     }
+                 }}>
+                      <Form.Item name="username" label="Username" rules={[{ required: true, message: 'Please input a persona username!' }]}><Input /></Form.Item>
+                      <Form.Item name="password" label="Password" rules={[{ required: true }]}><Input.Password /></Form.Item>
+                      <Form.Item name="remember"><Checkbox>Remember me</Checkbox></Form.Item>
+                 </Form>
+            </Modal>
+        );
+    }
+
+}
