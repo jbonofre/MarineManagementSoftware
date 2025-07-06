007bd213f57b72923aca29675ff6c32182463db8
JB Onofré <jb.onofre@dremio.com>
2025-07-06 17:21:26 +0200

    Champs utilisateur éditables

--- ui/src/utilisateurs.tsx
+++ ui/src/utilisateurs.tsx
@@ -1,16 +1,16 @@
-import { Card, Space, Table } from 'antd';
-import { UserOutlined } from '@ant-design/icons';
+import { Card, Space, Table, Select, Input, Button } from 'antd';
+import { UserOutlined, PlusCircleOutlined, DeleteOutlined } from '@ant-design/icons';
 
 const users = [
   {
       user: 'admin',
-      roles: [ 'admin' ],
+      roles: 'admin',
       password: 'admin',
       email: 'contact@msplaisance.com'
   },
   {
       user: 'accueil',
-      roles: [ 'accueil' ],
+      roles: 'accueil',
       password: 'accueil',
       email: 'contact@msplaisance.com'
   }
@@ -25,17 +25,38 @@ const columns = [
     {
         title: 'Roles',
         dataIndex: 'roles',
-        key: 'roles'
+        key: 'roles',
+        render: (_,record) => (
+            <Select style={{ width: 200 }} defaultValue={record.roles} options={[
+                { value: 'admin', label: 'admin' },
+                { value: 'accueil', label: 'accueil' },
+                { value: 'atelier', label: 'atelier' },
+                { value: 'magasin', label: 'magasin' }
+            ]} />
+        )
     },
     {
         title: 'Mot de Passe',
         dataIndex: 'password',
-        key: 'password'
+        key: 'password',
+        render: (_,record) => (
+            <Input.Password value={record.password} />
+        )
     },
     {
         title: 'E-mail',
         dataIndex: 'email',
-        key: 'email'
+        key: 'email',
+        render: (_,record) => (
+            <Input value={record.email} />
+        )
+    },
+    {
+        title: '',
+        key: 'action',
+        render: (_,record) => (
+            <Button icon={<DeleteOutlined/>} />
+        )
     }
 ];
 
@@ -44,6 +65,7 @@ export default function Utilisateurs(props) {
         <>
         <Card title={<Space><UserOutlined/> Utilisateurs</Space>}>
             <Table columns={columns} dataSource={users} />
+            <Button type="primary" icon={<PlusCircleOutlined/>}>Nouvel Utilisateur</Button>
         </Card>
         </>
     );
