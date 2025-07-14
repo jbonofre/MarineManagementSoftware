59a1ee2d404f788855f7f6a2819bc9f23ae08ff3
JB Onofré <jb.onofre@dremio.com>
2025-07-14 14:44:18 +0200

    Ajout d'information sur la société

--- ui/src/chantier.tsx
+++ ui/src/chantier.tsx
@@ -25,8 +25,17 @@ export default function Chantier(props) {
                     <Form.Item label="SIRET">
                         <Input value="790 458 616 00022"/>
                     </Form.Item>
+                    <Form.Item label="APE">
+                        <Input value="3315Z"/>
+                    </Form.Item>
+                    <Form.Item label="RCS">
+                        <Input value="790458616"/>
+                    </Form.Item>
                     <Form.Item label="Forme juridique">
                         <Input value="EURL"/>
+                    </Form.Item>
+                    <Form.Item label="Capital">
+                        <Input value="3500"/>
                     </Form.Item>
                     <Form.Item label="Numéro de TVA">
                         <Input value="FR79790458616"/>
@@ -35,7 +44,7 @@ export default function Chantier(props) {
                         <TextArea rows={6} value={adresse} />
                     </Form.Item>
                     <Form.Item label="Numéro de téléphone">
-                        <Input value="02 56 45 50 37" />
+                        <Input value="0256455037" />
                     </Form.Item>
                     <Form.Item label="Email">
                         <Input value="contact@msplaisance.com"/>
