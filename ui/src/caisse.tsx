98cab65edb9ee35360e0cc4dc7f4c07a526cc728
JB Onofré <jb.onofre@dremio.com>
2025-07-15 22:03:14 +0200

    Premiers éléments de la vue "nouvelle transaction"

--- ui/src/caisse.tsx
+++ ui/src/caisse.tsx
@@ -1,7 +1,25 @@
 import React, { useState } from 'react';
-import { Card, Space, Button, Table, Modal } from 'antd';
+import { Card, Space, Button, Table, Modal, Form, Input, Select, DatePicker, InputNumber, Transfer } from 'antd';
 import { DesktopOutlined, PlusCircleOutlined, EditOutlined, DeleteOutlined } from '@ant-design/icons';
 import { demo } from './workspace.tsx';
+
+const types = [
+  { value: 'Devis', label: 'Devis' },
+  { value: 'Commande', label: 'Commande' },
+  { value: 'Facture', label: 'Facture' },
+  { value: 'Facture payée', label: 'Facture payée' }
+];
+
+
+const reglement = [
+    { value: 'Espèce', label: 'Espèce' },
+    { value: 'Chéque', label: 'Chéque' },
+    { value: 'CB', label: 'CB' },
+    { value: 'Paypal', label: 'Paypal' },
+    { value: 'Virement', label: 'Virement' }
+];
+
+const { TextArea } = Input;
 
 const transactions = [
   {
@@ -72,10 +90,64 @@ const columns = [
   }
 ];
 
+function creernumero(length) {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
+
 function NouvelleTransaction(props) {
+
+    const numero = creernumero(10);
+
     return(
-        <Modal centered={true} mask={true} title='Nouvelle Transaction' open={props.openNew} okText='Sauvegarder' closable={true}>
-            <p>Nouvelle Transaction</p>
+        <Modal centered={true} mask={true} title='Nouvelle Transaction' open={props.openNew} okText='Sauvegarder'
+            closable={true} width={800} onCancel={() => props.setOpenNew(false)}>
+            <Form name="client" labelCol={{ span: 8 }}
+                 wrapperCol={{ span: 16 }}
+                 style={{ width: '80%' }}
+                 initialValues={{ remember: true }}>
+                <Form.Item label="Numéro">
+                    <Input value={numero} disabled={true} />
+                </Form.Item>
+                <Form.Item label="Type">
+                    <Select options={types} />
+                </Form.Item>
+                <Form.Item label="Client">
+                    <Input.Search />
+                </Form.Item>
+                <Form.Item label="Adresse du Client">
+                    <TextArea rows={6}/>
+                </Form.Item>
+                <Form.Item label="Date">
+                    <DatePicker />
+                </Form.Item>
+                <Form.Item label="Date de livraison">
+                    <DatePicker />
+                </Form.Item>
+                <Form.Item label="Date d'échéance">
+                    <DatePicker />
+                </Form.Item>
+                <Form.Item label="Montant HT">
+                    <InputNumber addonAfter="€" />
+                </Form.Item>
+                <Form.Item label="TVA">
+                    <InputNumber addonAfter="%" value="20.00" />
+                </Form.Item>
+                <Form.Item label="Montant TVA">
+                    <InputNumber addonAfter="€" />
+                </Form.Item>
+                <Form.Item label="Montant TTC">
+                    <InputNumber addonAfter="€" />
+                </Form.Item>
+                <Form.Item label="Articles">
+                    <Transfer showSearch />
+                </Form.Item>
+            </Form>
         </Modal>
     );
 }
@@ -109,7 +181,7 @@ export default function Caisse(props) {
     const [ numeroTransaction, setNumeroTransaction ] = useState(null);
     return(
       <>
-      <NouvelleTransaction openNew={openNew} />
+      <NouvelleTransaction openNew={openNew} setOpenNew={setOpenNew} />
       <DetailTransaction numeroTransaction={numeroTransaction} />
       <Card title={<Space><DesktopOutlined/> Guichet</Space>}>
         <Button type="primary" icon={<PlusCircleOutlined/>} onClick={() => setOpenNew(true) }>Nouvelle transaction</Button>
