ba1c0fd0987e0a4739c5022a7afb285e028b7c61
JB Onofré <jbonofre@apache.org>
2025-07-17 14:57:09 +0200

    Finalisation de la vue transaction détaillée

--- ui/src/caisse.tsx
+++ ui/src/caisse.tsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import { Card, Space, Button, Table, Modal, Form, Input, Select, DatePicker, InputNumber } from 'antd';
-import { DesktopOutlined, PlusCircleOutlined, EditOutlined, DeleteOutlined } from '@ant-design/icons';
+import { DesktopOutlined, PlusCircleOutlined, EditOutlined, DeleteOutlined, PrinterOutlined, SendOutlined } from '@ant-design/icons';
 import { demo } from './workspace.tsx';
+import dayjs from 'dayjs';
 
 const types = [
   { value: 'Ticket de Caisse', label: 'Ticket de Caisse' },
@@ -11,6 +12,15 @@ const types = [
   { value: 'Facture payée', label: 'Facture payée' }
 ];
 
+const articlesTable = [
+        { title: 'Reference', dataIndex: 'code', key: 'code' },
+        { title: 'Description', dataIndex: 'description', key: 'description' },
+        { title: 'Quantité', dataIndex: 'quantite', key: 'quantite' },
+        { title: 'Prix HT', dataIndex: 'prixht', key: 'prixht' },
+        { title: 'Remise', dataIndex: 'remise', key: 'remise' },
+        { title: 'TVA', dataIndex: 'tva', key: 'tva'},
+        { title: 'Prix TTC', dataIndex: 'prixttc', key: 'prixttc' }
+];
 
 const reglement = [
     { value: 'Espèce', label: 'Espèce' },
@@ -79,7 +89,8 @@ const columns = [
   {
     title: 'Numéro',
     dataIndex: 'numero',
-    key: 'numero'
+    key: 'numero',
+    render: (text,record) => <a>{text}</a>
   },
   {
     title: 'Type',
@@ -125,15 +136,6 @@ function creernumero(length) {
 function NouvelleTransaction(props) {
 
     const articles = [];
-    const articlesTable = [
-        { title: 'Reference', dataIndex: 'code', key: 'code' },
-        { title: 'Description', dataIndex: 'description', key: 'description' },
-        { title: 'Quantité', dataIndex: 'quantite', key: 'quantite' },
-        { title: 'Prix HT', dataIndex: 'prixht', key: 'prixht' },
-        { title: 'Remise', dataIndex: 'remise', key: 'remise' },
-        { title: 'TVA', dataIndex: 'tva', key: 'tva'},
-        { title: 'Prix TTC', dataIndex: 'prixttc', key: 'prixttc' }
-    ];
 
     const numero = creernumero(10);
 
@@ -214,18 +216,83 @@ function DetailTransaction(props) {
 
     var transactionType = null;
     var transactionNumero = null;
-
+    var transactionType = null;
+    var transactionClient = null;
+    var transactionAdresseClient = null;
+    var transactionDate = null;
+    var transactionDateLivraison = null;
+    var transactionDateEcheance = null;
+    var transactionArticles = [];
+    var transactionMontantHt = null;
+    var transactionTauxTva = null;
+    var transactionMontantTva = null;
+    var transactionMontantTtc = null;
 
     if (transaction != null) {
         transactionType = transaction.type;
         transactionNumero = transaction.numero;
+        transactionType = transaction.type;
+        transactionClient = transaction.client;
+        transactionAdresseClient = transaction.adresseclient;
+        transactionDate = transaction.date;
+        transactionDateLivraison = transaction.datelivraison;
+        transactionDateEcheance = transaction.dateecheance;
+        transactionArticles = transaction.items;
+        transactionMontantHt = transaction.montantht;
+        transactionTauxTva = transaction.tauxtva;
+        transactionMontantTva = transaction.montanttva;
+        transactionMontantTtc = transaction.montantttc;
     }
 
     return(
       <Modal centered={true} mask={true} title={<Space>{transactionType}{transactionNumero}</Space>}
             width={1024} open={open} closable={true} onCancel={() => props.setNumeroTransaction(null)}>
-
-      </Modal>
+            <Form name="client" labelCol={{ span: 3 }}
+                 wrapperCol={{ span: 21 }}
+                 style={{ width: '100%' }}
+                 initialValues={{ remember: true }}>
+                <Form.Item label="Numéro">
+                    <Input value={transactionNumero} disabled={true} />
+                </Form.Item>
+                <Form.Item label="Type">
+                    <Select options={types} defaultValue={transactionType} disabled={true} />
+                </Form.Item>
+                <Form.Item label="Client">
+                    <Input value={transactionClient} disabled={true} />
+                </Form.Item>
+                <Form.Item label="Adresse du Client">
+                    <TextArea rows={6} value={transactionAdresseClient} disabled={true}/>
+                </Form.Item>
+                <Form.Item label="Date">
+                    <DatePicker value={dayjs(transactionDate, 'DD-MM-YYYY')} format='DD-MM-YYYY' disabled={true} />
+                </Form.Item>
+                <Form.Item label="Date de livraison">
+                    <DatePicker value={dayjs(transactionDateLivraison, 'DD-MM-YYYY')} format='DD-MM-YYYY' disabled={true} />
+                </Form.Item>
+                <Form.Item label="Date d'échéance">
+                    <DatePicker value={dayjs(transactionDateEcheance, 'DD-MM-YYYY')} format='DD-MM-YYYY' disabled={true} />
+                </Form.Item>
+                <Form.Item label="Montant HT">
+                    <InputNumber value={transactionMontantHt} addonAfter="€" disabled={true} />
+                </Form.Item>
+                <Form.Item label="TVA">
+                    <InputNumber addonAfter="%" value={transactionTauxTva} disabled={true} />
+                </Form.Item>
+                <Form.Item label="Montant TVA">
+                    <InputNumber addonAfter="€" value={transactionMontantTva} disabled={true} />
+                </Form.Item>
+                <Form.Item label="Montant TTC">
+                    <InputNumber addonAfter="€" value={transactionMontantTtc} disabled={true} />
+                </Form.Item>
+                <Form.Item label="Articles">
+                    <Table columns={articlesTable} dataSource={transactionArticles} />
+                </Form.Item>
+            </Form>
+            <Space>
+                <Button type="primary" icon={<PrinterOutlined/>}>Imprimer</Button>
+                <Button type="primary" icon={<SendOutlined/>}>Envoyer par e-mail</Button>
+            </Space>
+        </Modal>
     );
 }
 
