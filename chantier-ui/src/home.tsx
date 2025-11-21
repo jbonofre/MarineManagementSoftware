961ab72ebab63cede8d0c93a8406d51710ed3c61
JB Onofré <jbonofre@apache.org>
2025-11-21 15:11:55 +0100

    Modification de la home page.

--- chantier-ui/src/home.tsx
+++ chantier-ui/src/home.tsx
@@ -1,21 +1,25 @@
-import { Row, Col, Card, Calendar } from 'antd';
+import React from 'react';
+import { Typography, Row, Col, Card } from 'antd';
+import { SmileOutlined } from '@ant-design/icons';
+
+const { Title, Paragraph } = Typography;
 
 export default function Home() {
-
-    return(
-        <Row gutter={10}>
-            <Col span={10}>
-                <Card title="Messages" style={{ height: 800 }}>
-                    <p><b>Accueil (23-06-2025)</b> Rappeler John Doeuf</p>
-                    <p><b>Chantier (22-06-2025)</b> Rappel Mercury, mise à jour de l'échéancier d'entretien</p>
-                </Card>
-            </Col>
-            <Col span={14}>
-                <Card title="Planning" style={{ height: 800 }}>
-                    <Calendar />
-                </Card>
-            </Col>
-        </Row>
+    return (
+        <Card bordered={false} style={{ marginBottom: 24, background: '#fafafa' }}>
+            <Row align="middle">
+                <Col flex="40px">
+                    <SmileOutlined style={{ fontSize: 40, color: '#1890ff' }} />
+                </Col>
+                <Col flex="auto" style={{ paddingLeft: 16 }}>
+                    <Title level={2} style={{ marginBottom: 0 }}>Bienvenue sur Marine Management Software</Title>
+                    <Paragraph type="secondary" style={{ marginBottom: 0 }}>
+                        Gérez vos clients, bateaux, moteurs, remorques et plus encore en toute simplicité.<br />
+                        Utilisez le menu latéral pour naviguer entre les différentes fonctionnalités.<br />
+                        Nous vous souhaitons une excellente expérience sur notre plateforme.
+                    </Paragraph>
+                </Col>
+            </Row>
+        </Card>
     );
-
 }
