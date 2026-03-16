108557b7aa0c74eda373e48ab2d467c2bfb6b304
JB Onofré <jbonofre@apache.org>
2026-03-16 15:03:01 +0100

    feat: ajouter une image de fond sur les pages de connexion client-ui et technicien-ui

--- technicien-ui/src/login.tsx
+++ technicien-ui/src/login.tsx
@@ -35,45 +35,65 @@ export default function Login({ setUser }: LoginProps) {
     };
 
     return (
-        <Modal
-            centered
-            mask={false}
-            title={<Space>Espace Technicien <Image width={50} src="./logo.png" preview={false} /></Space>}
-            open
-            okText="Se connecter"
-            cancelText="Effacer"
-            closable={false}
-            confirmLoading={loading}
-            onOk={() => form.submit()}
-            onCancel={() => form.resetFields()}
-        >
-            <Form
-                name="login"
-                form={form}
-                labelCol={{ span: 8 }}
-                wrapperCol={{ span: 16 }}
-                autoComplete="off"
-                onFinish={handleLogin}
-                onKeyUp={(event: React.KeyboardEvent) => {
-                    if (event.key === 'Enter') {
-                        form.submit();
-                    }
-                }}
+        <div style={{
+            position: 'fixed',
+            top: 0,
+            left: 0,
+            width: '100vw',
+            height: '100vh',
+            backgroundImage: 'url(./login-background.jpg)',
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            backgroundRepeat: 'no-repeat',
+        }}>
+            <div style={{
+                position: 'absolute',
+                top: 0,
+                left: 0,
+                width: '100%',
+                height: '100%',
+                backgroundColor: 'rgba(0, 30, 60, 0.5)',
+            }} />
+            <Modal
+                centered
+                mask={false}
+                title={<Space>Espace Technicien <Image width={50} src="./logo.png" preview={false} /></Space>}
+                open
+                okText="Se connecter"
+                cancelText="Effacer"
+                closable={false}
+                confirmLoading={loading}
+                onOk={() => form.submit()}
+                onCancel={() => form.resetFields()}
             >
-                <Form.Item
-                    name="email"
-                    label="Email"
-                    rules={[
-                        { required: true, message: "L'email est requis" },
-                        { type: 'email', message: "Format d'email invalide" },
-                    ]}
+                <Form
+                    name="login"
+                    form={form}
+                    labelCol={{ span: 8 }}
+                    wrapperCol={{ span: 16 }}
+                    autoComplete="off"
+                    onFinish={handleLogin}
+                    onKeyUp={(event: React.KeyboardEvent) => {
+                        if (event.key === 'Enter') {
+                            form.submit();
+                        }
+                    }}
                 >
-                    <Input />
-                </Form.Item>
-                <Form.Item name="motDePasse" label="Mot de passe">
-                    <Input.Password />
-                </Form.Item>
-            </Form>
-        </Modal>
+                    <Form.Item
+                        name="email"
+                        label="Email"
+                        rules={[
+                            { required: true, message: "L'email est requis" },
+                            { type: 'email', message: "Format d'email invalide" },
+                        ]}
+                    >
+                        <Input />
+                    </Form.Item>
+                    <Form.Item name="motDePasse" label="Mot de passe">
+                        <Input.Password />
+                    </Form.Item>
+                </Form>
+            </Modal>
+        </div>
     );
 }
